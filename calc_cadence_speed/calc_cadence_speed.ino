ebebeba28d3dc1c3c73a663f084a9dc0694c42ae
HanKyeol Kyung <kghnkl0103@gmail.com>
2020-03-19 23:38:12 +0900

    int로 부족한것같아 long으로변경

--- calc_cadence_speed/calc_cadence_speed.ino
+++ calc_cadence_speed/calc_cadence_speed.ino
@@ -6,9 +6,9 @@ LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 char
 volatile int rpmcount[2] = {0, 0};
 const float circumference = 2200.0; //바퀴둘레 : 2200mm
 const int hall_num[2] = {12, 8}; //홀센서 갯수
-unsigned int lcd_update_before = 0;
+unsigned long lcd_update_before = 0;
 const int lcd_update_interval = 300; // 300ms마다 업데이트
-unsigned int rpm_update_before[2] = {0, 0};
+unsigned long rpm_update_before[2] = {0, 0};
 const int rpm_update_interval = 1000; //300ms마다 업데이트
 float rpm_arr[2] = {0.0, 0.0};
 
@@ -65,7 +65,7 @@ void lcd_update(float rpm[]) {
     lcd.print("rpm");      // lcd 모니터에 rpm 표시해주기
     Serial.print("speed : ");
     lcd.setCursor(5, 3);
-    lcd.print("   ");    //LCD에 새로운 rpm값을 계속 표시해주기 위해서 이전에 표시되었던 내용을 지워주는 과정
+    lcd.print("    ");    //LCD에 새로운 rpm값을 계속 표시해주기 위해서 이전에 표시되었던 내용을 지워주는 과정
     lcd.setCursor(5, 3);   // 5,3에서 글쓰기 시작
     float bikespeed = (rpm[1] * circumference * 60 ) / (1000 * 1000);
     lcd.print((int)bikespeed);
